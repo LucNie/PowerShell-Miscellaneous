13bda2d2c5b48aca8329f05aa4f8c6c3b2e71460
Galaxial <90603307+LucNie@users.noreply.github.com>
2022-04-29 10:11:37 +0200

    Create npmUpdate.ps1

---
+++ npmUpdate.ps1
@@ -0,0 +1,29 @@
+$arr = Get-ChildItem ./ | 
+       Where-Object {$_.PSIsContainer} | 
+       Foreach-Object {$_.Name}
+
+$confirm = "N"
+$confirm = Read-Host 'Node modules ? Install(I) Delete(D) Update(U)'
+
+If($confirm -eq "I"){
+    For($i=0;$i -lt $arr.length ;$i++) 
+    { 
+       Write-Output "node-modules install : $($arr[$i])" 
+       start-process powershell.exe -argument "-nologo -noprofile -command cd ./'$($arr[$i])' ; npm i ;exit"
+    }
+}elseif($confirm -eq "D"){
+    For($i=0;$i -lt $arr.length ;$i++) 
+    { 
+        Write-Output "node-modules Remove : $($arr[$i])" 
+        start-process powershell.exe -argument "-nologo -noprofile -command cd ./'$($arr[$i])' ; Remove-Item -path .\node_modules -recurse ;exit"
+    }
+}elseif($confirm -eq "U"){
+    For($i=0;$i -lt $arr.length ;$i++) 
+    { 
+        Write-Output "node-modules Update : (DISABLE) $($arr[$i])" 
+        start-process powershell.exe -argument "-nologo -noprofile -command cd ./'$($arr[$i])' ; echo DISABLE ;exit"
+    }
+}else{
+    Write-Output "good bye"
+    exit
+}
