d9530f181a8e5b8b10ecdfc285cde763c154b4ab
Galaxial <90603307+LucNie@users.noreply.github.com>
2022-06-16 09:10:39 +0200

    adding a "Travail" template

--- Folder structure basic.ps1
+++ Folder structure basic.ps1
@@ -1,93 +1,111 @@
 $confirm = "N"
-$confirm = Read-Host 'Crée un squelette type GOAT structure ? [C]Complette [S]Standart [L]Leger [N]no '
+$confirm = Read-Host 'Crée un squelette type GOAT structure ? [C]Complette [S]Standart [L]Leger [T]Travail [N]no '
 
-If($confirm -eq "C") 
-{
+If ($confirm -eq "C") {
 
-mkdir "000 archive"
-mkdir "00 cache"
-mkdir "01 Programmes"
-mkdir "02 Jeux"
-mkdir "03 Programmation"
+    mkdir "000 archive"
+    mkdir "00 cache"
+    mkdir "01 Programmes"
+    mkdir "02 Jeux"
+    mkdir "03 Programmation"
 
-Set-Location "./03 Programmation"
-mkdir "30 Cache"
-mkdir "31 Unity"
-mkdir "32 Visual studio"
-mkdir "33 Visual studio code"
-mkdir "34 Git"
-Set-Location "../"
+    Set-Location "./03 Programmation"
+    mkdir "30 Cache"
+    mkdir "31 Unity"
+    mkdir "32 Visual studio"
+    mkdir "33 Visual studio code"
+    mkdir "34 Git"
+    Set-Location "../"
 
-mkdir "04 Video"
-Set-Location "./04 Video"
-mkdir "40 Cache"
-mkdir "41 Raw"
-mkdir "42 Editer"
-mkdir "43 Fond vert"
-mkdir "44 Sond"
-mkdir "45 Music"
-mkdir "46 Fini"
+    mkdir "04 Video"
+    Set-Location "./04 Video"
+    mkdir "40 Cache"
+    mkdir "41 Raw"
+    mkdir "42 Editer"
+    mkdir "43 Fond vert"
+    mkdir "44 Sond"
+    mkdir "45 Music"
+    mkdir "46 Fini"
 
-Set-Location "../"
-
-
-mkdir "05 Images"
-Set-Location "./05 Images"
-mkdir "50 Cache"
-mkdir "51 Raw"
-mkdir "52 Editer"
-mkdir "53 Fond vert"
-mkdir "54 Sond"
-mkdir "55 Music"
-mkdir "56 Fini"
-Set-Location "../"
-
-mkdir "06 Music"
-Set-Location "./06 Music"
-mkdir "60 Cache"
-mkdir "61 playlist"
-Set-Location "../"
-mkdir "07 Ecole"
-mkdir "08 travail"
-
-mkdir "09 projet"
-Set-Location "./09 projet"
-mkdir "90 Cache"
-Set-Location "../"
-
-mkdir "10 documents"
-
-mkdir "11 animes"
-Set-Location "./11 animes"
-mkdir "110 Cache"
-mkdir "111 Raw"
-mkdir "112 non fini"
-mkdir "113 Fini"
-Set-Location "../"
+    Set-Location "../"
 
 
+    mkdir "05 Images"
+    Set-Location "./05 Images"
+    mkdir "50 Cache"
+    mkdir "51 Raw"
+    mkdir "52 Editer"
+    mkdir "53 Fond vert"
+    mkdir "54 Sond"
+    mkdir "55 Music"
+    mkdir "56 Fini"
+    Set-Location "../"
+
+    mkdir "06 Music"
+    Set-Location "./06 Music"
+    mkdir "60 Cache"
+    mkdir "61 playlist"
+    Set-Location "../"
+    mkdir "07 Ecole"
+    mkdir "08 travail"
+
+    mkdir "09 projet"
+    Set-Location "./09 projet"
+    mkdir "90 Cache"
+    Set-Location "../"
+
+    mkdir "10 documents"
+
+    mkdir "11 animes"
+    Set-Location "./11 animes"
+    mkdir "110 Cache"
+    mkdir "111 Raw"
+    mkdir "112 non fini"
+    mkdir "113 Fini"
+    Set-Location "../"
 
 
-
-}elseif ($confirm -eq "L"){
+}
+elseif ($confirm -eq "L") {
    
-mkdir "000 archive"
-mkdir "00 cache"
-mkdir "01 Programmes"
-mkdir "02 Jeux"
-mkdir "03 Programmation"
-mkdir "04 Video"
-mkdir "05 Images"
-mkdir "06 Music"
-mkdir "07 Ecole"
-mkdir "08 travail"
-mkdir "09 projet"
-mkdir "10 documents"
-mkdir "11 animes"
+    mkdir "000 archive"
+    mkdir "00 cache"
+    mkdir "01 Programmes"
+    mkdir "02 Jeux"
+    mkdir "03 Programmation"
+    mkdir "04 Video"
+    mkdir "05 Images"
+    mkdir "06 Music"
+    mkdir "07 Ecole"
+    mkdir "08 travail"
+    mkdir "09 projet"
+    mkdir "10 documents"
+    mkdir "11 animes"
 
 
 
 
-}else{
-  exit  
+}
+elseif ($confirm -eq "T") {
+    
+    mkdir "000 archive"
+    mkdir "00 cache"
+    mkdir "01 Programmes"
+    mkdir "03 Programmation"
+
+    Set-Location "./03 Programmation"
+    mkdir "30 Cache"
+    mkdir "31 Unity"
+    mkdir "32 Visual studio"
+    mkdir "33 Visual studio code"
+    mkdir "34 Git"
+    Set-Location "../"
+
+    mkdir "04 Video"
+    mkdir "05 Images"
+    mkdir "09 projet"
+    mkdir "10 documents"
+}
+else {
+    exit
 }
